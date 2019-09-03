070cc13256b758298d686dcf1118b3a3de2393b6
asifamingov <asif.amin@dsiti.qld.gov.au>
2019-09-03 23:01:01 +1000

    sadf

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -6,6 +6,7 @@ describe(
   () => {
     let page
     beforeAll(async () => {
+      jest.setTimeout(7000000);
       page = await global.__BROWSER__.newPage()
       await page.goto('http://localhost:8086/docs/components.html', {waitUntil: 'load', timeout: 700000})
     }, timeout)
