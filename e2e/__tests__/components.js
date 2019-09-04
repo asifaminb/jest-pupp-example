435cae43cf4692177085307ee4111f58cfc62730
AsifAmin <asif_udr@yahoo.com>
2019-09-04 11:33:14 +1000

    sadf

--- e2e/__tests__/components.js
+++ e2e/__tests__/components.js
@@ -1,29 +1,31 @@
 const puppeteer = require('puppeteer')
+const ct = require('../config/constants');
+
 let browser
 let page
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
-    executablePath:
-      "./node_modules/chromium/lib/chromium/chrome-linux/chrome",
+    executablePath: ct.CHROME_PATH,
   })
   page = await browser.newPage()
 })
 
-describe('SWE tests', () => {
+describe('SWE templates testing', () => {
   test('has search input', async () => {
-    await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('http://localhost:8086/docs/components.html', { waitUntil: 'networkidle0' })
+    await page.setViewport({ width: ct.BT_XL, height: 800 })
+    await page.goto(`${ct.APP_URL}/docs/components.html`, { waitUntil: 'networkidle0' })
     await page.type('input[id=qg-search-query]', 'jobs', {delay: 20})
     await page.waitForSelector('.listbox li')
     const list = (await page.$$('.listbox li')).length;
     expect(list).toBeGreaterThan(0);
-  }, 10000)
+  }, ct.TIMEOUT)
 
   test('shows search results after search input', async () => {
-    await page.waitForSelector('#page-feedback-useful')
+    const pfel = '#page-feedback-useful';
+    await page.waitForSelector(pfel)
     expect(await page.evaluate('window.getComputedStyle(document.getElementById(\'qg-page-feedback\')).getPropertyValue("display")')).toBe('none');
-    (await page.$('#page-feedback-useful')).click();
+    (await page.$(pfel)).click();
     await page.waitFor(3000);
     expect(await page.evaluate('window.getComputedStyle(document.getElementById(\'qg-page-feedback\')).getPropertyValue("display")')).not.toBe('none');
   })
