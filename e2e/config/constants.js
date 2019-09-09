61960f635628db98b916e5f3aa2fad143dff5f66
AsifAmin <asif_udr@yahoo.com>
2019-09-09 10:32:55 +1000

    sadf

--- e2e/config/constants.js
+++ e2e/config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APP_URL: "http://localhost:8087",
+  APP_URL: "http://localhost:8088",
   CHROME_PATH: "./node_modules/chromium/lib/chromium/chrome-linux/chrome",
   BT_SM: 576,
   BT_MD: 768,
